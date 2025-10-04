93f1e2852ad4547100c5f4112141366fba09df85
Alejo Garlati <gabrielgarlati@gmail.com>
2025-10-04 16:50:59 -0300

    Agrego variables de color (paleta) y cosas varias de prueba

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import './Home.css'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Hello putos</h1>
-    </div>
+    <body className="bg-gray-200">
+      <div >
+        <section className="container text-center mt-50">
+          <h1 className="text-5xl">¡Bienvenido!</h1>
+        </section>
+      </div>
+    </body>
   )
 }
 
