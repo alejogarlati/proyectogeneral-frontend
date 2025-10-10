0f8e40058b820d5721eaae8ce85c75a87f79994e
Alejo Garlati <gabrielgarlati@gmail.com>
2025-10-09 22:17:58 -0300

    creacion de Home y paleta de colores

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,10 +3,20 @@ import './Home.css'
 
 const Home = () => {
   return (
-    <body className="bg-gray-200">
-      <div >
-        <section className="container text-center mt-50">
-          <h1 className="text-5xl">¡Bienvenido!</h1>
+    <body>
+      <div>
+        <section className="flex flex-col gap-5 items-center justify-center h-screen text-center mt-50">
+          <div className='logo-container'>
+            <img src="./src/assets/logo_kiosco_ampus.png" alt="Logo Kiosco Campus" className="mx-auto mb-4" />
+            </div>
+            <h1 className="text-5xl">¡Bienvenido!</h1>
+          <div className='flex flex-row items-center justify-center gap-5 home-buttons'>
+              <button className='btn btn-1'>Iniciar Sesión</button>
+              <button className='btn btn-2'>Registrarse</button>
+          </div>
+          <div className='footer'>
+            <p className="footer-text">© 2025 P*A(2). Todos los derechos reservados.</p>
+          </div>
         </section>
       </div>
     </body>
