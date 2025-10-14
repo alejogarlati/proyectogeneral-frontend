21f90fb3a69886c8147db9a7f7171ea104a11278
Alejo Garlati <gabrielgarlati@gmail.com>
2025-10-14 20:14:50 -0300

    Agrego hooks de react-hook-form al FormLogin1

--- src/components/LoginForm/FormLogin1.jsx
+++ src/components/LoginForm/FormLogin1.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import { Footer } from "../Footer/Footer.jsx";
+import {useForm} from "react-hook-form";
 
 export const FormLogin1 = () => {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    reset,
+    resetField,
+    setValue
+  } = useForm();
+
+
+  const onSubmit = handleSubmit((data) => console.log(data));
+
   return (
     <div className="form-login-1 flex flex-col gap-12 items-center justify-center h-[92dvh]">
       <div className="bg-white p-18 flex flex-col gap-8 rounded-xl shadow-xl">
         <h1 className="text-5xl text-(--color-secondary) max-w-85 text-center">
           Formulario de Inicio de Sesión
         </h1>
-        <form className="flex flex-col gap-6 text-(--color-secondary)">
+        <form onSubmit={onSubmit} className="flex flex-col gap-6 text-(--color-secondary)">
           <fieldset className="flex flex-col gap-2">
             <label htmlFor="nombreUsuario" className="ml-2 text-xs">
               Usuario
@@ -18,6 +33,11 @@ export const FormLogin1 = () => {
               type="text"
               placeholder="Tu nombre de usuario"
               className="text-sm font-normal p-2 border rounded bg-(--color-armony) p-2 focus:outline-(--color-secondary) transition"
+              {...register("nombreUsuario", { 
+                  required: "Nombre de usuario inválido", 
+                  maxLength: {value: 20, message: "El nombre de usuario no puede exceder los 20 caracteres"}
+                  })
+              }
             />
           </fieldset>
           <fieldset className="flex flex-col gap-2">
@@ -29,6 +49,10 @@ export const FormLogin1 = () => {
               type="text"
               placeholder="Tu contraseña"
               className="text-sm font-normal p-2 border rounded bg-(--color-armony) p-2 focus:outline-(--color-secondary) transition"
+              {...register("passUsuario", {
+                required: "Contraseña inválida",
+                maxLength: {value: 20, message: "La contraseña no puede exceder los 20 caracteres"}
+              })}
             />
           </fieldset>
           <button
