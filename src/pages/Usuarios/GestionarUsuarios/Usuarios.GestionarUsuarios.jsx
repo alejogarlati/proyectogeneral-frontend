b5d6c3ea28fe5d1743a7c25ee5381392054a9329
Alejo Garlati <gabrielgarlati@gmail.com>
2025-10-26 22:23:52 -0300

    layout base de Gestion de Usuarios

--- src/pages/Usuarios/GestionarUsuarios/Usuarios.GestionarUsuarios.jsx
+++ src/pages/Usuarios/GestionarUsuarios/Usuarios.GestionarUsuarios.jsx
@@ -1,14 +1,38 @@
 import { getUsers } from "@/services/services";
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLoaderData } from "react-router-dom";
+import { Input } from "@/components/ui/input"
 
 export const UsuariosGestionarUsuarios = () => {
   const navigate = useNavigate();
 
+  const usersData = useLoaderData();
+
+  const handleOnChange = (e) => {
+    // console.log(e.target.value)
+    const results = usersData.filter((item) => item.userName == e.target.value)
+    console.log("Resultados :", results)
+  }
+
   return (
-    <div>
-      
-      <button className='btn p-2 bg-(--primary)' onClick={() => navigate("/")}>Ir al Home</button>
+    <div className="grid grid-cols-12 w-full gap-4 h-screen">
+      <div className="col-span-6 flex flex-col items-center justify-start w-full gap-2 rounded rounded-xl p-4">
+        <div className="searchbar w-full">
+          <Input
+          type="text"
+          placeholder="Buscar"
+          id="searchInput"
+          name="searchInput"
+          onChange={handleOnChange}
+          />
+        </div>
+        <div className="usersTable w-full">
+          <h2>Table de usuarios</h2>
+        </div>
+      </div>
+      <div className="col-span-6 bg-(--primary)">
+        <p> Lorem ipsum </p>
+      </div>
     </div>
   );
 };
@@ -16,9 +40,8 @@ export const UsuariosGestionarUsuarios = () => {
 export const loader = async () => {
   const usuarios = await getUsers();
   if (usuarios.data.success) {
-    console.log("Listado de Usuarios: ", usuarios.data.data); 
+    // console.log("Listado de Usuarios: ", usuarios.data.data);
+    return usuarios.data.data
   }
   return null;
 };
-
-
